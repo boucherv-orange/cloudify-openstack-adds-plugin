eac24a08a3b22bacce22cb251e3c10d34422be1c
boucherv <valentin.boucher@orange.com>
2016-07-05 17:30:20 +0200

    add operation

--- designate_plugin/recordsets.py
+++ designate_plugin/recordsets.py
@@ -7,6 +7,7 @@ Created on Fri Jul  1 15:11:36 2016
 
 
 from cloudify import ctx
+from cloudify.decorators import operation
 import designateclient.v2.client as dgclient
 from keystoneauth1 import session as keystone_session
 

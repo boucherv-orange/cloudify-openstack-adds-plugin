8e9989b469d1fc81b3bf01f88f25b4e29ba05762
boucherv <valentin.boucher@orange.com>
2016-07-07 10:38:33 +0200

    fix get record

--- designate_plugin/recordsets.py
+++ designate_plugin/recordsets.py
@@ -15,20 +15,18 @@ from keystoneauth1 import session as keystone_session
 def get_client(dns_ip):
     session = keystone_session.Session()
     return dgclient.Client(endpoint_override="http://" + dns_ip + ":9001/v2", session=session)
-    
 
 def create_record(designate_client, zone_name, recordset_name, type_, new_record):
     recordsets = designate_client.recordsets.list(zone_name)
     recordset_exist = False
     for recordset in recordsets:
-        if recordset['name'] == recordset_name:
+        if recordset['name'] == recordset_name and recordset['type'] == type_:
             recordset_exist = True
+            records = recordset.get('records')
 
     if not recordset_exist:
         return designate_client.recordsets.create(zone_name, recordset_name, type_, [new_record])
     else:
-        recordset = designate_client.recordsets.get(zone_name, recordset_name)
-        records = recordset.get('records')
         records.append(new_record)
         data = {
                 'name': recordset_name,
@@ -50,8 +48,9 @@ def delete_record(designate_client, zone_name, recordset_name, type_, new_record
                 'records': records
         }
         return designate_client.recordsets.update(zone_name, recordset_name, data)
+
 @operation
-def add_node(domain, record_ip, other_roles, other_records, **kwargs):
+def add_node(domain, other_roles, other_records, record_ip="", **kwargs):
     zone_name = domain + "."
     instance_name = ctx.source.instance.id
     role = re.split(r'_',instance_name)[0]
@@ -80,7 +79,7 @@ def add_node(domain, record_ip, other_roles, other_records, **kwargs):
             ctx.logger.debug(delete_record(designate_client, zone_name, recordset_name, type_, record_value))        
 
 @operation
-def del_node(domain, record_ip, other_roles, other_records, **kwargs):
+def del_node(domain, other_roles, other_records, record_ip="", **kwargs):
     zone_name = domain + "."
     instance_name = ctx.source.instance.id
     role = re.split(r'_',instance_name)[0]
